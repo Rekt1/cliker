7cacf9be879d6e508be2123f0fc2280830f2e0ca
Антон <31995761+Rekt1@users.noreply.github.com>
2020-06-07 20:50:40 +0300

    Update cliker.py

--- cliker.py
+++ cliker.py
@@ -8,4 +8,8 @@ while i in range(1):
 	num = pg.prompt("Сколько надо сделать кликов?","Кликер","10")
 	time_click = pg.prompt("С каким интервалом кликать(в секундах)?","Кликер","1")
 	time = pg.prompt("Через сколько начать кликать(в секундах)?","Кликер","5")
-	pg.click(clicks=int(num), interval=int(time_click))
+	try:
+		pg.click(clicks=int(num), interval=int(time_click))
+	except TypeError:
+		print("Bye :3")
+		i += 1
